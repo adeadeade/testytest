e04be94436ed4cf6babb391c9fe65c7e90bd1311
adeadeade <adeade@orange.net>
2014-10-27 14:11:16 +0000

    Test 2

--- gitTest/gitTest/Form1.vb
+++ gitTest/gitTest/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+
+        Dim varStr As String = ""
+        MsgBox("")
+
+    End Sub
 End Class
